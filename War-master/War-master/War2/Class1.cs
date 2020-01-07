2ef96ce6589c608fb1c7dd96751fe070743609b1
tynaminh <57113105+tynaminh@users.noreply.github.com>
2020-01-07 14:28:56 -0600

    multiple casino games

---
+++ War-master/War-master/War2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Class1
+{
+    {
+        Random numbergemerator = new Random();
+        int num1 = numbergenerator.Next(1, 11);
+        int num2 = numbergenerator.Next(1, 11);
+        int score = 0;
+
+        Console.WriteLine("Whats " + num1 + " times " + num2 + "?");
+        var answer = Convert.ToInt32(Console.ReadLine());
+
+        if (answer == num1 * num2) 
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Your card is higher, you won that round!");
+            Console.ResetColor();
+            ++score;
+            Console.WriteLine("Your score: " + score);
+        } else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Uh oh, your card is smaller than the opponent's, try again.");
+            Console.ResetColor();
+          ++score;
+            Console.WriteLine("Your score: " + score);
+        }
+        go to start;
+
+    }
+}
